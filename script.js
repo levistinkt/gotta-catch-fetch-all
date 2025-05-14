b16be7414d123ecf5cf31b033d10048393ac3303
levistinkt <sdestinkerd@gmail.com>
2025-05-14 10:52:11 +0200

    update sprites
    animated gifs !

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ document.getElementById('pokemon-button').addEventListener('click', function(){
 
         document.getElementById('pokemon-name').innerHTML = data.name
 
-        document.getElementById('pokemon-img').src = data.sprites.front_default
-        document.getElementById('pokemon-img-back').src = data.sprites.back_default
-        document.getElementById('pokemon-img-shiny').src = data.sprites.front_shiny
-        document.getElementById('pokemon-img-shiny-back').src = data.sprites.back_shiny
+        document.getElementById('pokemon-img').src = data.sprites.versions["generation-v"]["black-white"].animated.front_default
+        document.getElementById('pokemon-img-back').src = data.sprites.versions["generation-v"]["black-white"].animated.back_default
+        document.getElementById('pokemon-img-shiny').src = data.sprites.versions["generation-v"]["black-white"].animated.front_shiny
+        document.getElementById('pokemon-img-shiny-back').src = data.sprites.versions["generation-v"]["black-white"].animated.back_shiny
 
         document.getElementById('pokemon-search').value = ""
 
